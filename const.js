9b1109b562914aec1ee8f1a3f7811096bf710867
syedibrahimt <syedibrahim.developer@gmail.com>
2025-07-18 10:19:06 +0530

    removing key

--- const.js
+++ const.js
@@ -1,3 +1,1 @@
-export const OPENAI_API_KEY =
-  "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"
 export const OPENAI_API_URL = "https://api.openai.com/v1/realtime/sessions"
